71b2004f1cf054427e9254c606a5dece43385fa6
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2020-12-01 14:15:32 -0300

    planes

--- js/scrpits.js
+++ js/scrpits.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoader", function hola() {
-  console.log("hola");
-});
+const tabItems = document.querySelectorAll(".tabs__item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+const tabContainer = document.querySelector(".container-tab");
+
+// =====================================================================
+// listen for tab click in icons
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
+
+// Select tab-content where Grid photos is
+function selectItem(e) {
+  e.preventDefault();
+  removeBorder();
+  removeShow();
+  // createTabs();
+  // Add class with red Border
+  this.classList.add("tabs__item--border");
+  // Asign the ID to a variable so we can call it back
+  const idItem = this.id;
+  // get item content from DOM
+  // console.log("selectItem -> idItem", idItem)
+  const tabContentItem = document.querySelector(`#${idItem}-content`);
+  tabContentItem.classList.add("show");
+}
+
+// Remove red border when it is not selected
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tabs__item--border"));
+}
+// Remove SHOW class so we can see the content
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
