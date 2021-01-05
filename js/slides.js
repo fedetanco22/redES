0911d811ddfffe47b72e6b61a325f31d420713f3
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-01-05 16:31:37 -0300

    Contacto

--- js/slides.js
+++ js/slides.js
@@ -25,10 +25,37 @@ var slider = new Glider(document.querySelector(".carousel__lista"), {
     },
   ],
 });
+var slider2 = new Glider(document.querySelector(".carousel__lista--clientes"), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  rewind: true,
+  // arrows: {
+  //   prev: ".carousel__anterior-2",
+  //   next: ".carousel__siguient-2",
+  // },
+  responsive: [
+    {
+      breakpoint: 567,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: ".carousel__indicadores",
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      },
+    },
+  ],
+});
 // });
 slideAutoPaly(slider, ".glider");
+slideAutoPaly(slider2, ".glider");
 
-function slideAutoPaly(glider, selector, delay = 5000, repeat = true) {
+function slideAutoPaly(glider, selector, delay = 3000, repeat = true) {
   let autoplay = null;
   const slidesCount = glider.track.childElementCount;
   let nextIndex = 1;
