9a2e737a18f2d72f63f27f1c5b7cacea09ccdc7e
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-08 16:20:39 -0300

    sliderOneDone

--- js/slides.js
+++ js/slides.js
@@ -1,134 +1,30 @@
-// window.addEventListener("load", function () {
-
-var slider = new Glider(document.querySelector(".carousel__lista"), {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  rewind: true,
-  arrows: {
-    prev: ".carousel__anterior",
-    next: ".carousel__siguiente",
-  },
-
-  responsive: [
-    {
-      breakpoint: 776,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: ".carousel__indicadores",
-      },
-    },
-    {
-      breakpoint: 567,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: ".carousel__indicadores",
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      },
-    },
-  ],
-});
-
-var slider2 = new Glider(document.querySelector(".carousel__lista--clientes"), {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  rewind: true,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 567,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 2,
-        dots: ".carousel__indicadores",
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      },
-    },
-  ],
-});
-
-// });
-// slideAutoPaly(slider, ".glider");
-slideAutoPaly(slider2, ".glider");
-
-function slideAutoPaly(glider, selector, delay = 3000, repeat = true) {
-  let autoplay = null;
-  const slidesCount = glider.track.childElementCount;
-  let nextIndex = 1;
-  let pause = true;
-
-  function slide() {
-    autoplay = setInterval(() => {
-      if (nextIndex >= slidesCount) {
-        if (!repeat) {
-          clearInterval(autoplay);
-        } else {
-          nextIndex = 0;
-        }
-      }
-      glider.scrollItem(nextIndex++);
-    }, delay);
-  }
-
-  slide();
-
-  var element = document.querySelector(selector);
-  element.addEventListener(
-    "mouseover",
-    (event) => {
-      if (pause) {
-        clearInterval(autoplay);
-        pause = false;
-      }
-    },
-    300
-  );
-
-  element.addEventListener(
-    "mouseout",
-    (event) => {
-      if (!pause) {
-        slide();
-        pause = true;
-      }
-    },
-    300
-  );
-}
-
 $(document).ready(function () {
-  $("#autoWidth").lightSlider({
-    item: 1,
+  $(".center").slick({
+    // center: true,
+    autoplay: true,
+    arrows: false,
+    mobileFirst: true,
+    centerMode: true,
     autoWidth: true,
-    auto: true,
-    loop: true,
-    pause: 3000,
-    focuscenter: true,
+    slidesToShow: 1,
+    variableWidth: true,
+    infinite: true,
     responsive: [
       {
-        breakpoint: 768,
-        settings: {
-          item: 1,
-          slideMove: 1,
-          slideMargin: 6,
-        },
+        breakpoint: 996,
+        settings: {},
       },
     ],
-    onSliderLoad: function () {
-      $("#autoWidth").removeClass("cS-hidden");
-    },
+  });
+});
+
+$(document).ready(function () {
+  $(".carousel__lista").slick({
+    mobileFirst: true,
+    centerMode: true,
+    prevArrow:
+      '<span aria-label="Anterior" class="carousel__anterior"><i class="fas fa-chevron-left carousel__anterior--chevron"></i></span>',
+    nextArrow:
+      ' <span aria-label="Siguiente" class="carousel__siguiente"><i class="fas fa-chevron-right carousel__anterior--chevron"></i>',
   });
 });
