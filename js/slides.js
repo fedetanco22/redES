83a089df78a2cc695305df1014f72c0eafef1813
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-09 19:11:49 -0300

    Canva 992pxr

--- js/slides.js
+++ js/slides.js
@@ -1,3 +1,5 @@
+// TODO -- RESPONSIVE (PUT BREAKPOINTS)
+
 $(document).ready(function () {
   $(".center").slick({
     // center: true,
@@ -12,30 +14,58 @@ $(document).ready(function () {
     infinite: true,
     responsive: [
       {
-        breakpoint: 996,
+        breakpoint: 992,
         settings: {},
       },
     ],
   });
 });
 
+// TODO -- RESPONSIVE (PUT BREAKPOINTS)
+
 $(document).ready(function () {
   $(".center2").slick({
-    // autoplay: true,
+    autoplay: true,
     arrows: true,
     mobileFirst: true,
     centerMode: true,
     centerPadding: "50px",
     slidesToShow: 1,
     slidesToScroll: 1,
-    // fade: true,
-    // focusOnSelect: true,
-    // autoWidth: true,
     variableWidth: true,
     infinite: true,
     prevArrow:
       '<span aria-label="Anterior" class="carousel__anterior"><i class="fas fa-chevron-left carousel__anterior--chevron"></i></span>',
     nextArrow:
       ' <span aria-label="Siguiente" class="carousel__siguiente"><i class="fas fa-chevron-right carousel__anterior--chevron"></i>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {},
+      },
+    ],
+  });
+});
+
+// TODO -- RESPONSIVE (PUT BREAKPOINTS)
+
+$(document).ready(function () {
+  $(".center3").slick({
+    autoplay: true,
+    arrows: false,
+    mobileFirst: true,
+    centerMode: true,
+    centerPadding: "50px",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoWidth: true,
+    variableWidth: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {},
+      },
+    ],
   });
 });
