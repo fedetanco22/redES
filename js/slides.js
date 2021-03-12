d0d16af6e7c5b9a971572bed8f46441216ff4b5f
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-03-12 17:21:08 -0300

    IN-PROCESS- Slider 992+

--- js/slides.js
+++ js/slides.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
   $(".center2--lg").slick({
     autoplay: true,
     arrows: true,
-    dots: true,
+    // dots: true,
 
     centerMode: false,
     slidesToShow: 1,
@@ -58,18 +58,9 @@ $(document).ready(function () {
     variableWidth: true,
     infinite: true,
     prevArrow:
-      '<span aria-label="Anterior" class="carousel__anterior"><i class="fas fa-chevron-left carousel__anterior--chevron"></i></span>',
+      '<span aria-label="Anterior" class="carousel__anterior"><i class="fas fa-arrow-circle-left carousel__anterior--chevron"></i></span>',
     nextArrow:
-      ' <span aria-label="Siguiente" class="carousel__siguiente"><i class="fas fa-chevron-right carousel__anterior--chevron"></i>',
-    responsive: [
-      {
-        breakpoint: 776,
-        settings: {
-          centerMode: true,
-          centerPadding: "50px",
-        },
-      },
-    ],
+      ' <span aria-label="Siguiente" class="carousel__siguiente"><i class="fas fa-arrow-circle-right carousel__anterior--chevron"></i>',
   });
 });
 
